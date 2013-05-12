ff49f79daa296178780de61c972064a6af289c19
Weltraumschaf <ich@weltraumschaf.de>
2013-05-12 18:10:23 +0200

    Set up scafold for JSP and JErsey serving.

---
+++ server/src/main/java/org/lafayette/server/resources/IndexResource.java
@@ -0,0 +1,31 @@
+/*
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com>
+ */
+package org.lafayette.server.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+@Path("/")
+public class IndexResource {
+
+    @Produces(MediaType.TEXT_PLAIN)
+    @GET public String indexAsHtml() {
+        return "Lafayette Server\n\nHello World!\n";
+    }
+
+}
