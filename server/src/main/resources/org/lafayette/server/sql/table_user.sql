7bead90f52c018cf8905d31c42c1c60cc7bdfef4
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2013-05-17 13:06:12 +0200

    Introduce HSQLDB for tests.

---
+++ server/src/main/resources/org/lafayette/server/sql/table_user.sql
@@ -0,0 +1,18 @@
+/*
+ * LICENSE
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+create table user (
+    id int not null,
+    loginName varchar(255) not null,
+    hashedPassword varchar(255) not null,
+    salt varchar(255) not null,
+    primary key(id),
+    unique(loginName)
+);
---
+++ server/src/test/resources/org/lafayette/server/sql/data_user.sql
@@ -0,0 +1,13 @@
+/*
+ * LICENSE
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+insert into user values (1, "Foo", "b9f46238b289f23ba807973840655032", "Oih0mei7");
+insert into user values (2, "Bar", "043bd227eaa879d438e7c1dfea568bc9", "AiZuur1Y");
+insert into user values (3, "Baz", "aa82cc74b4a932c06d4ea5a9ac38cf5e", "Eng7ovej");
