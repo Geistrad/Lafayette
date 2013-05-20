f737882733cb5562f8497d63a3f5196436f52579
Weltraumschaf <ich@weltraumschaf.de>
2013-05-20 13:22:13 +0200

    Tests for registry setters.

--- server/src/test/java/org/lafayette/server/RegistryTest.java
+++ server/src/test/java/org/lafayette/server/RegistryTest.java
@@ -11,11 +11,16 @@
  */
 package org.lafayette.server;
 
+import de.weltraumschaf.commons.Version;
+import java.sql.Connection;
+import java.util.Properties;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.lafayette.server.config.ServerConfig;
+import org.lafayette.server.db.NullConnection;
 
 /**
  * Tests for {@link Registry}.
@@ -58,6 +63,34 @@ public class RegistryTest {
     public void setVersion_throwsExceptionIfParameterIsNull() {
         thrown.expect(NullPointerException.class);
         sut.setVersion(null);
+    }
+
+    @Test
+    public void setDatabase() {
+        final Connection db = new NullConnection();
+        sut.setDatabase(db);
+        assertThat(sut.getDatabase(), is(sameInstance(db)));
+    }
+
+    @Test
+    public void setServerConfig() {
+        final ServerConfig config = new ServerConfig(new Properties());
+        sut.setServerConfig(config);
+        assertThat(sut.getServerConfig(), is(sameInstance(config)));
+    }
+
+    @Test
+    public void setStage() {
+        final Stage stage = new Stage();
+        sut.setStage(stage);
+        assertThat(sut.getStage(), is(sameInstance(stage)));
+    }
+
+    @Test
+    public void setVersion() {
+        final Version version = new Version("");
+        sut.setVersion(version);
+        assertThat(sut.getVersion(), is(sameInstance(version)));
     }
 
 }
