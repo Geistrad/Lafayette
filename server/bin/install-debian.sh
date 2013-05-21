1a98c5f6c8688719fb93820f109169b47897920d
Weltraumschaf <ich@weltraumschaf.de>
2013-05-21 20:41:12 +0200

    Fix appending to apt source list.

--- server/bin/install-debian.sh
+++ server/bin/install-debian.sh
@@ -5,16 +5,21 @@
 #
 
 # Add Jenkins (http://pkg.jenkins-ci.org/debian/)
+echo "Add Jenkins sources to apt sources..."
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
-echo "" > /etc/apt/sources.list
-echo "# Jenkins source" > /etc/apt/sources.list
-echo "deb http://pkg.jenkins-ci.org/debian binary/" > /etc/apt/sources.list
+echo "" >> /etc/apt/sources.list
+echo "# Jenkins source" >> /etc/apt/sources.list
+echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.list
 
 # Update sources
+echo "Update apt sources..."
 apt-get update
+echo "Upgrade packages..."
+apt-get -y upgrade 
 
 # Install packages.
-apt-get install \
+echo "Install packages..."
+apt-get -y install \
     apt-file \
     etckeeper \
     git \
@@ -30,4 +35,7 @@ apt-get install \
     unzip \
 
 # Update apt files.
+echo "Update apt-file..."
 apt-file update
+
+echo "Done :)"
