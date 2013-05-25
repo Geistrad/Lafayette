d76300a3d1e4cfd815b1c0e38e7fae51225cb870
Weltraumschaf <ich@weltraumschaf.de>
2013-05-26 01:30:49 +0200

    Tests for user object.

--- server/src/test/java/org/lafayette/server/domain/UserTest.java
+++ server/src/test/java/org/lafayette/server/domain/UserTest.java
@@ -13,9 +13,11 @@
 package org.lafayette.server.domain;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 /**
+ * Tests for {@link User}.
  *
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
@@ -23,14 +25,90 @@ public class UserTest {
 
     @Test
     public void testHashCode() {
+        final User u1 = new User(1, "foo", "bar", "baz");
+        final User u2 = new User(1, "foo", "bar", "baz");
+        final User u3 = new User(2, "snafu", "bar", "baz");
+
+        assertThat(u1.hashCode(), is(equalTo(u1.hashCode())));
+        assertThat(u1.hashCode(), is(equalTo(u2.hashCode())));
+        assertThat(u2.hashCode(), is(equalTo(u1.hashCode())));
+        assertThat(u2.hashCode(), is(equalTo(u2.hashCode())));
+
+        assertThat(u1.hashCode(), is(not(equalTo(u3.hashCode()))));
+        assertThat(u2.hashCode(), is(not(equalTo(u3.hashCode()))));
+
+        final Role r1 = new Role(1, "foo", "bar");
+        u1.addRole(r1);
+        assertThat(u1.hashCode(), is(not(equalTo(u2.hashCode()))));
+        u2.addRole(r1);
+        assertThat(u1.hashCode(), is(equalTo(u2.hashCode())));
     }
 
     @Test
+    @SuppressWarnings("IncompatibleEquals")
     public void testEquals() {
+        final User u1 = new User(1, "foo", "bar", "baz");
+        final User u2 = new User(1, "foo", "bar", "baz");
+        final User u3 = new User(2, "snafu", "bar", "baz");
+
+        //CHECKSTYLE:OFF
+        assertThat(u1.equals(null), is(equalTo(false)));
+        assertThat(u1.equals(""), is(equalTo(false)));
+        //CHECKSTYLE:ON
+
+        assertThat(u1.equals(u1), is(equalTo(true)));
+        assertThat(u1.equals(u2), is(equalTo(true)));
+        assertThat(u2.equals(u1), is(equalTo(true)));
+        assertThat(u2.equals(u2), is(equalTo(true)));
+
+        assertThat(u1.equals(u3), is(equalTo(false)));
+        assertThat(u2.equals(u3), is(equalTo(false)));
+
+        final Role r1 = new Role(1, "foo", "bar");
+        u1.addRole(r1);
+        assertThat(u1.equals(u2), is(equalTo(false)));
+        u2.addRole(r1);
+        assertThat(u1.equals(u2), is(equalTo(true)));
     }
 
     @Test
     public void testToString() {
+        final User u1 = new User(1, "foo", "bar", "baz");
+        assertThat(u1.toString(), is(equalTo("User{id=1, loginName=foo, hashedPassword=bar, salt=baz, roles=[]}")));
+
+        final Role r1 = new Role(1, "foo", "bar");
+        u1.addRole(r1);
+        assertThat(u1.toString(), is(equalTo("User{id=1, loginName=foo, hashedPassword=bar, salt=baz, roles=["
+                + "Role{id=1, name=foo, description=bar}"
+                + "]}")));
+
+        final Role r2 = new Role(2, "baz", "bar");
+        u1.addRole(r2);
+        assertThat(u1.toString(), is(equalTo("User{id=1, loginName=foo, hashedPassword=bar, salt=baz, roles=["
+                + "Role{id=2, name=baz, description=bar}, "
+                + "Role{id=1, name=foo, description=bar}"
+                + "]}")));
+    }
+
+    @Test
+    public void addGetAndRemoveRoles() {
+        final User u1 = new User(1, "foo", "bar", "baz");
+        assertThat(u1.getRoles(), hasSize(0));
+
+        final Role r1 = new Role(1, "foo", "bar");
+        assertThat(u1.hasRole(r1), is(equalTo(false)));
+        u1.addRole(r1);
+        assertThat(u1.hasRole(r1), is(equalTo(true)));
+        assertThat(u1.getRoles(), hasSize(1));
+        assertThat(u1.getRoles(), contains(r1));
+
+        final Role r2 = new Role(2, "baz", "bar");
+        assertThat(u1.hasRole(r2), is(equalTo(false)));
+        u1.addRole(r2);
+        assertThat(u1.hasRole(r1), is(equalTo(true)));
+        assertThat(u1.hasRole(r2), is(equalTo(true)));
+        assertThat(u1.getRoles(), hasSize(2));
+        assertThat(u1.getRoles(), containsInAnyOrder(r1, r2));
     }
 
 }
