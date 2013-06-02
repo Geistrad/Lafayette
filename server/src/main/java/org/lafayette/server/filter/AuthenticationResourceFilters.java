458de91ef8db9b34f9728796963af8f74ff51517
Weltraumschaf <ich@weltraumschaf.de>
2013-06-02 18:33:09 +0200

    Add tests for resource filter factory.

--- server/src/main/java/org/lafayette/server/filter/AuthenticationResourceFilters.java
+++ server/src/main/java/org/lafayette/server/filter/AuthenticationResourceFilters.java
@@ -20,6 +20,7 @@ import javax.servlet.ServletContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.ext.Provider;
+import org.apache.commons.lang3.Validate;
 
 /**
  * Filters the request/response for authentication.
@@ -31,9 +32,13 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class AuthenticationResourceFilters implements ResourceFilterFactory {
 
-    @Context
+    /**
+     * Provided by Jersey servlet.
+     */
     private ServletContext servlet;
-    @Context
+    /**
+     * Provided by Jersey servlet.
+     */
     private HttpHeaders headers;
 
     @Override
@@ -46,5 +51,28 @@ public class AuthenticationResourceFilters implements ResourceFilterFactory {
 
         return Collections.<ResourceFilter>singletonList(new SecurityContextFilterAnonymous());
     }
+
+    /**
+     * Injection point for servlet context.
+     *
+     * @param servlet must not be {@code null}
+     */
+    @Context
+    public void setServlet(final ServletContext servlet) {
+        Validate.notNull(servlet);
+        this.servlet = servlet;
+    }
+
+    /**
+     * Injection point for request headers.
+     *
+     * @param headers must not be {@code null}
+     */
+    @Context
+    public void setHeaders(final HttpHeaders headers) {
+        Validate.notNull(headers);
+        this.headers = headers;
+    }
+
 
 }
