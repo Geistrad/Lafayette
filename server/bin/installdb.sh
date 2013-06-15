73d9265da4043f5dc13fc4e7cf60422b40f1463b
Weltraumschaf <ich@weltraumschaf.de>
2013-06-15 17:09:33 +0200

    Add install script for database.

---
+++ server/bin/installdb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "${1}" == "" ] ; then
+	PASSWORD="${1}"
+else
+	echo "Error: Give MySQL root password as first argument!"
+	exit 1
+fi
+
+SQL_DIR="../model/src/resources/org/lafayette/server/sql"
+mysql -uroot -p "${PASSWORD}" < "${SQL_DIR}/setup_db_user.sql"
+
+DB_USER="lafayette"
+DB_PASSWORD="ooleiTh1"
+DB_TABLES="table_user table_role"
+DB_DATA-"data_user data_role"
+
+for file in "${DB_TABLES}"
+do
+		mysql -u"${DB_USER}" -p"${DB_PASSWORD}" < "${SQL_DIR}/${file}.sql"
+done
+
+for file in "${DB_DATA}"
+do
+		mysql -u"${DB_USER}" -p"${DB_PASSWORD}" < "${SQL_DIR}/${file}.sql"
+done
